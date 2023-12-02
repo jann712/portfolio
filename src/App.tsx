9a964d56f9aa8d9a3b8a62def23349d557fbcca3
jann712 <joao.augustonn1@gmail.com>
2023-12-02 20:37:22 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <div className="h-screen bg-cyan-50 mx-auto text-center">
+      <div className="mt-5">
+        <p>Hello</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
