44961593d5f424e7b6490807ab7a4ffd049f04fc
jann712 <joao.augustonn1@gmail.com>
2023-12-03 12:49:17 -0300

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 function App() {
-
   return (
     <div className="h-screen bg-cyan-50 mx-auto text-center">
-      <div className="mt-5">
-        <p>Hello</p>
+      <div className="pt-5 mb-5">
+        <h3 className="text-4xl">Portfólio</h3>
+      </div>
+      <div className="text-xl px-48">
+        <p>
+          Olá, bem-vindo ao meu portfólio. <br/>Me chamo João Augusto e sou
+          desenvolvedor Full-stack utilizando principalmente as tecnologias
+          TypeScript e Node.js.
+        </p>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
